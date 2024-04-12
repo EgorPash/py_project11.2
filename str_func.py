356f6348eaafdd1ae0271baf8e67d4349fe7480b
egorpash <2021104302@pnu.edu.ru>
2024-04-12 18:41:59 +0200

    add first function

---
+++ str_func.py
@@ -0,0 +1,7 @@
+def capitalize_all_letters(input_string):
+    return input_string.upper()
+
+# Пример использования функции:
+input_string = "Пример строки с разным регистром"
+result = capitalize_all_letters(input_string)
+print(result)
